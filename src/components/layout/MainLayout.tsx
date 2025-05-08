1badbfeadbcf2da979b3ee331fd2301aa863de2b
solaiman-siam <solaimansiam93@gmail.com>
2025-05-08 22:16:14 +0600

    first commit

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,54 @@
+import { Layout, Menu } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { createElement } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
+function MainLayout() {
+
+
+    const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
+        (icon, index) => ({
+          key: String(index + 1),
+          icon: createElement(icon),
+          label: `nav ${index + 1}`,
+        }),
+      );
+
+
+  return (
+    <Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={(broken) => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="demo-logo-vertical" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+    </Sider>
+    <Layout>
+      <Header style={{ padding: 0, background: 'white' }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div
+          style={{
+            padding: 24,
+            minHeight: 360,
+            background: 'white',
+            borderRadius: '10px',
+          }}
+        >
+          The Main Content should go there
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  </Layout>
+  )
+}
+
+export default MainLayout
